20cf7b0b8e9e6f6dbb30347b6abc3cfeea533c26
RostSuslov <suslovros@gmail.com>
2024-06-19 21:02:31 +0300

    feat: header done

--- dev/assets/js/custom.js
+++ dev/assets/js/custom.js
@@ -1,1 +1,13 @@
-  
+const burger = document.querySelector('.btn-burger');
+const mobileContainer = document.querySelector('.mobile-container')
+const body = document.querySelector('body')
+
+function toggleClass() {
+    mobileContainer.classList.toggle('show')
+    burger.classList.toggle('active')
+    body.classList.toggle('overflow-hidden')
+}
+
+burger.addEventListener('click', toggleClass)
+
+ 
